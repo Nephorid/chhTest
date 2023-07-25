a8f63accef88da9e66a80f0093d589ef6c798ba9
Ali Alizada <92538155+Nephorid@users.noreply.github.com>
2023-07-25 22:05:07 +0400

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -1,13 +1,9 @@
-// Rakamlar dizisi
 const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-// Sayfayı yüklediğimizde captcha oluştur
 document.addEventListener('DOMContentLoaded', createCaptcha);
 
-// Doğrulamayı kontrol etmek için kullanıcının girdiği rakamları sakla
 let userDigits = '';
 
-// Doğrulama yapılacak rakamları karıştır ve ekrana bastır
 function createCaptcha() {
     userDigits = '';
     const captchaContainer = document.getElementById('captcha');
@@ -22,23 +18,21 @@ function createCaptcha() {
     });
 }
 
-// Rakamları rastgele karıştırmak için kullanılacak yardımcı fonksiyon
 function shuffleArray(array) {
     return array.sort(() => Math.random() - 0.5);
 }
 
-// Kullanıcının girdiği rakamları doğrulama için saklayan fonksiyon
 function addToUserDigits(digit) {
     userDigits += digit;
 
-    // Eğer kullanıcı doğru sırayla tıklarsa, 1234 gibi beklenen bir dizi elde ederiz
+  
     if (userDigits.length === digits.length) {
         if (userDigits === digits.join('')) {
-            alert('Doğrulama başarılı!'); // Doğrulama başarılı mesajı
-            createCaptcha(); // Yeni bir captcha oluştur
+            alert('Doğrulama başarılı!'); 
+            createCaptcha(); 
         } else {
-            alert('Doğrulama başarısız! Tekrar deneyin.'); // Doğrulama başarısız mesajı
-            userDigits = ''; // Kullanıcının girdiği rakamları sıfırla
+            alert('Doğrulama başarısız! Tekrar deneyin.'); 
+            userDigits = ''; 
         }
     }
 }
